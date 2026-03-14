60307db8f8cbdaef6ab329e1722c30c03ee2cad9
mxtrd <maxtrod27@gmail.com>
2026-03-14 20:21:43 +0500

    chore(architecture): scaffold page-centric structure and base styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import { MainPage } from './MainPage.tsx'
+// import { StrictMode } from 'react'
+import App from '@/app'
 
 createRoot(document.getElementById('root')!).render(
-  <MainPage />
+    <App />
 )
