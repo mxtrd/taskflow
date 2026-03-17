bfbdef9652309073a91dbd7d286a9c5e2fadd259
mxtrd <maxtrod27@gmail.com>
2026-03-17 20:10:48 +0500

    fix(pr): resolve review/ci issue

--- src/pages/login-page/ui/LoginPage.tsx
+++ src/pages/login-page/ui/LoginPage.tsx
@@ -1,10 +1,9 @@
 import BaseLayout from '@/app/layouts/base-layout'
-import styles from './LoginPage.module.scss'
 
 const LoginPage = () => {
   return (
     <BaseLayout title="Taskflow" description="Taskflow - login page">
-      
+      <div>LoginPage</div>
     </BaseLayout>
   )
 }
