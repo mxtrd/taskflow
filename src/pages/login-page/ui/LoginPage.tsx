007a074a9ec09c21b99f1f10c8466ac1488111e7
mxtrd <maxtrod27@gmail.com>
2026-03-17 20:44:35 +0500

    feat(login): scaffold login page ui

--- src/pages/login-page/ui/LoginPage.tsx
+++ src/pages/login-page/ui/LoginPage.tsx
@@ -1,9 +1,41 @@
-import BaseLayout from '@/app/layouts/base-layout'
+import BaseLayout from "@/app/layouts/base-layout"
+import baseStyles from "@/app/styles/base.module.scss"
+import styles from "./LoginPage.module.scss"
 
 const LoginPage = () => {
   return (
     <BaseLayout title="Taskflow" description="Taskflow - login page">
-      <div>LoginPage</div>
+      <section className={styles.login}>
+        <div className={baseStyles.container}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Task Manager</h1>
+            <form className={styles.loginForm} action="#">
+              <div className={styles.column}>
+                <label className={styles.label} htmlFor="useremail">
+                  Email
+                </label>
+                <input className={styles.input} type="email" name="email" id="useremail" />
+              </div>
+              <div className={styles.column}>
+                <label className={styles.label} htmlFor="userpassword">
+                  Password
+                </label>
+                <input
+                  className={styles.input}
+                  type="password"
+                  name="password"
+                  id="userpassword"
+                />
+              </div>
+              <div className={styles.column}>
+                <button className={styles.button} type="submit">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
     </BaseLayout>
   )
 }
