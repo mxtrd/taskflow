7a7ea19cc6aa13379b058c38c8b2ed8318fc201c
mxtrd <maxtrod27@gmail.com>
2026-03-18 16:55:53 +0500

    feat(routing): add react-router core routes for stage 2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,24 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import LoginPage from "@/pages/login-page"
+import BoardsPage from "@/pages/boards-page"
+import BoardPage from "@/pages/board-page"
 import TaskPage from "@/pages/task-page"
+
+const ProfilePage = () => <div>Profile Page</div>
+const NotFoundPage = () => <div>404 Page not found</div>
 
 const App = () => {
   return (
-      <TaskPage />
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/boards" element={<BoardsPage />} />
+      <Route path="/boards/:boardId" element={<BoardPage />} />
+      <Route path="/boards/:boardId/tasks/:taskId" element={<TaskPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+
+      <Route path="/" element={<Navigate to="/boards" replace />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   )
 }
 
