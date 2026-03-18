66287f848155814da324710e06f463e4edc42cbb
mxtrd <maxtrod27@gmail.com>
2026-03-18 16:56:44 +0500

    feat(routing): add react-router core routes for stage 2 (#15)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 // import { StrictMode } from 'react'
 import App from "@/app"
 
 createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
+  </BrowserRouter>,
 )
