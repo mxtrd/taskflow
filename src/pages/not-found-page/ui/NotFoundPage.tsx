3c0d37af88d69a22f03a64dd3127bf025ef39043
mxtrd <maxtrod27@gmail.com>
2026-04-16 19:15:42 +0500

    feat(404): responsive layout for 404 page

--- src/pages/not-found-page/ui/NotFoundPage.tsx
+++ src/pages/not-found-page/ui/NotFoundPage.tsx
@@ -1,14 +1,19 @@
 import BaseLayout from "@/app/layouts/base-layout"
 import baseStyles from '@/app/styles/base.module.scss'
+import Button from '@/shared/ui/button'
 import styles from './NotFoundPage.module.scss'
 
 const NotFoundPage = () => {
   return (
     <BaseLayout title='Taskflow' description='Taskflow - 404 page'>
-      <section className={styles.boards}>
+      <section className={baseStyles.section}>
         <div className={baseStyles.container}>
-          <div className={baseStyles.content}>
+          <div className={`${baseStyles.content} ${styles.notFoundContent}`}>
             <h1 className={styles.title}>Page not found</h1>
+            <p className={styles.text}>The page you are trying to open does not exist.</p>
+            <Button className={styles.button} to='/boards'>
+              Go to boards
+            </Button>
           </div>
         </div>
       </section>
